c12cd95ca6062cd277411dd14b9fe2b8dc87f403
Alexxx180 <alexx120702@gmail.com>
2024-07-31 18:11:35 +0300

    is windows os check fix

--- lua/core/startup/godot.lua
+++ lua/core/startup/godot.lua
@@ -1,7 +1,7 @@
 -- Enable neovim to be the external editor for Godot, if the cwd has a project.godot file
 if vim.fn.filereadable(vim.fn.getcwd() .. '/project.godot') == 1 then
   local address = './godot.pipe'
-  if vim.fn.has 'win32' == 1 then address = '127.0.0.1:6004' end
+  if is_windows then address = '127.0.0.1:6004' end
   vim.fn.serverstart(address)
 end
 
