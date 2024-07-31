8e31577ff295177277ce650ea3bac12922068d3e
Alexxx180 <alexx120702@gmail.com>
2024-07-31 22:19:53 +0300

    fix lsp features mapping

--- lua/plugins/lsp/commands/attach.lua
+++ lua/plugins/lsp/commands/attach.lua
@@ -1,5 +1,4 @@
 lsp_on_attach = function(event)
-    require("core.mapping.enable.lsp")
-    map_linked_features(get_lsp_features(), event.buf)
+    map_linked_features('lsp', get_lsp_features(), event.buf)
     cursor_features(event)
 end
