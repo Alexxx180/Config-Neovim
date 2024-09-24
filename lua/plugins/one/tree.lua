6802d2b6935bd5792a9defcf536993993d60e053
Alexxx180 <Alexxx180@github.com>
2024-09-24 11:41:10 -0400

    implementing variants for common interactables logic

--- lua/plugins/one/tree.lua
+++ lua/plugins/one/tree.lua
@@ -2,7 +2,7 @@ function config_nvimtree()
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
-    local treeview = { width = 30 }
+    local treeview = { width = 45 }
 
     require("nvim-tree").setup({ view = treeview })
     map_features(mappings['tree'], nvimtree)
